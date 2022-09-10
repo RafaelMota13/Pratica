4279ff45f07a958a4bdc81a0e8e20e9fe8ede2f8
Rafael <faelmota7@gmail.com>
2022-09-09 21:15:02 -0300

    TESTE

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.ForegroundColor = ConsoleColor.Red;
+Console.BackgroundColor = ConsoleColor.White;
+string nome = "Rafael"
+                .ToUpper()
+                .Replace("E", "xx")
+                .Replace("RA","WZY");
+string Sobrenome = "Mota";
+
+string nomeCompleto =$"{nome}\n{Sobrenome.ToLower()}";
+
+string citacao = $"A Etec é a melhor escola.";
+Console.WriteLine(nomeCompleto);
+Console.WriteLine(citacao);
+
+
+#pragma warning disable CA1416
+
+Console.Beep(1132,350);
+Console.Beep(1485,350);
+Console.Beep(1650,350);
+Console.Beep(1759,550);
+Console.Beep(1759,350);
+Console.Beep(1759,350);
+Console.ResetColor();
